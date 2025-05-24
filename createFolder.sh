29f82e6864b985ffe99417158e3d46b54f454891
Jonas <jonasper19@gmail.com>
2025-05-23 23:59:29 -0300

    create folder

---
+++ createFolder.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#Check input
+if [ -z "$1" ]; then
+	echo "How to use: ./createFolder.sh <folder name>"
+	exit 1
+fi
+
+FOLDERNAME=$1
+
+mkdir -p $FOLDERNAME
+mkdir -p $FOLDERNAME/src
+mkdir -p $FOLDERNAME/includes
+cat > "$FOLDERNAME/Makefile" << EOF
+NAME =
+TESTS = 5
+
+CC = c++
+FLAGS = -g3 -Wall -Werror -Wextra -std=c++98
+
+INCLUDE = includes
+SRC =	main.cpp
+SRCS = \$(addprefix src/, \$(SRC))
+OBJS = \$(SRCS:.cpp=.o)
+
+RM = rm -f
+
+all: \$(NAME)
+
+\$(NAME): \$(OBJS)
+	@\$(CC) \$(FLAGS) -I\$(INCLUDE) -o \$(NAME) \$(OBJS)
+
+src/%.o: src/%.cpp
+	@echo "\033[1;32mCompiling	->	\$<\033[0m"
+	@\$(CC) \$(FLAGS) -I\$(INCLUDE) -c \$< -o \$@
+
+run: all
+	@valgrind ./\$(NAME)
+
+test: all
+	@for i in \$(shell seq 1 \$(TESTS)); do \\
+		valgrind ./\$(NAME) \$\$i; \\
+		if [ \$\$i -eq 3 ]; then \\
+			cat test_shrubbery; \\
+		fi; \\
+	done
+
+clean:
+	@echo "\033[1;32m===========================================\033[0m"
+	@echo "\033[1;32m		OBJECTS REMOVED						\033[0m"
+	@echo "\033[1;32m===========================================\033[0m"
+	@\$(RM) \$(OBJS)
+
+fclean: clean
+	@echo "\033[1;32m===========================================\033[0m"
+	@echo "\033[1;32m		\$(NAME) REMOVED						\033[0m"
+	@echo "\033[1;32m===========================================\033[0m"	
+	@\$(RM) \$(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re run
+EOF

5f8ffc0dc63f1ff22c8ccfad539f87ebdffb82c6
Jonas Alberto <157934840+oJonasRtz@users.noreply.github.com>
2025-05-23 23:27:29 -0300

    Update ClassTemplate.cpp

--- ClassTemplate.cpp
+++ ClassTemplate.cpp
@@ -2,23 +2,23 @@
 
 ClassTemplate::ClassTemplate()
 {
-	std::cout << BRIGHT_GREEN "constructor called\n" RESET;
+	std::cout << BRIGHT_GREEN "ClassTemplate: constructor called\n" RESET;
 }
 ClassTemplate::ClassTemplate(const ClassTemplate &other)
 {
-	std::cout << BRIGHT_GREEN "copy constructor called\n" RESET;
+	std::cout << BRIGHT_GREEN "ClassTemplate: copy constructor called\n" RESET;
 	*this = other;
 }
 ClassTemplate&	ClassTemplate::operator=(const ClassTemplate &other)
 {
-	std::cout << BRIGHT_GREEN "assignment operator\n" RESET;
+	std::cout << BRIGHT_GREEN "ClassTemplate: assignment operator\n" RESET;
 	if (this != &other)
 		/*	copy	*/
 	return (*this);
 }
 ClassTemplate::~ClassTemplate()
 {
-	std::cout << BRIGHT_RED "destructor called\n" RESET;
+	std::cout << BRIGHT_RED "ClassTemplate: destructor called\n" RESET;
 }
 
 std::ostream	&operator<<(std::ostream &out, const ClassTemplate &other)

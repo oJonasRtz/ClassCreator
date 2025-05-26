e0afe9b91324d74496ad20930739bf71ff9df2a0
Jonas Alberto <157934840+oJonasRtz@users.noreply.github.com>
2025-05-26 18:19:39 -0300

    Update createClass.sh

--- createClass.sh
+++ createClass.sh
@@ -6,9 +6,13 @@ if [ $# -lt 2 ]; then
 	exit 1
 fi
 
+#Name and destiny
 CLASSNAME=$1
 FOLDER=$2
 
+#Colours
+ORANGE='\033[38;5;208m'
+RESET='\033[0m'
 
 #copy files
 cp ClassTemplate.hpp ${CLASSNAME}.hpp
@@ -19,7 +23,7 @@ sed -i "s/ClassTemplate/${CLASSNAME}/g" ${CLASSNAME}.hpp
 sed -i "s/ClassTemplate/${CLASSNAME}/g" ${CLASSNAME}.cpp
 sed -i "s/CLASSTEMPLATE_HPP/$(echo ${CLASSNAME} | tr '[:lower:]' '[:upper:]')_HPP/g" ${CLASSNAME}.hpp
 
-echo "${CLASSNAME} class successfuly created!"
+echo -e "Class: ${ORANGE}${CLASSNAME}${RESET} successfuly created!"
 
 mv ${CLASSNAME}.hpp ${FOLDER}/includes
 mv ${CLASSNAME}.cpp ${FOLDER}/src

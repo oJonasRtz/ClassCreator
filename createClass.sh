995c48de9b9aff1949e7833d3a5f5ab7aae64215
Jonas Alberto <157934840+oJonasRtz@users.noreply.github.com>
2025-05-26 18:11:40 -0300

    Update createClass.sh

--- createClass.sh
+++ createClass.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 #Check input
-if [ -z "$1" ]; then
-	echo "How to use: ./createClass.sh <class name>"
+if [ $# -lt 2 ]; then
+	echo "How to use: ./createClass.sh <class name> <folder>"
 	exit 1
 fi
 
 CLASSNAME=$1
+FOLDER=$2
+
 
 #copy files
 cp ClassTemplate.hpp ${CLASSNAME}.hpp
@@ -17,7 +19,7 @@ sed -i "s/ClassTemplate/${CLASSNAME}/g" ${CLASSNAME}.hpp
 sed -i "s/ClassTemplate/${CLASSNAME}/g" ${CLASSNAME}.cpp
 sed -i "s/CLASSTEMPLATE_HPP/$(echo ${CLASSNAME} | tr '[:lower:]' '[:upper:]')_HPP/g" ${CLASSNAME}.hpp
 
+echo "Class ${CLASSNAME} successfuly created!"
+
 mv ${CLASSNAME}.hpp ${FOLDER}/includes
 mv ${CLASSNAME}.cpp ${FOLDER}/src
-
-echo "Class ${CLASSNAME} successfuly created!"
